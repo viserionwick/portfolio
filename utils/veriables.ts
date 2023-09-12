95f4388feb25017ab724ec717089aaaccd1833ec
viserionwick <viserionwick@gmail.com>
2023-09-12 12:02:19 -0700

    veriables.ts fix

--- utils/veriables.ts
+++ utils/veriables.ts
@@ -1,7 +1,7 @@
 export const WEBSITE_URL =  process.env.NODE_ENV === "development" ?
                             "http://localhost:3000/"
                             : process.env.NODE_ENV === "production" ?
-                            "http://viserionwick.vercel.app/"
+                            process.env.WEBSITE_URL
                             : ""
 
 export const GRAPHQL_API_ENDPOINT = `${WEBSITE_URL}/api/graphql`;
