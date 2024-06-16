20dabb67496c2db5f46f18be441fe924f3cff786
viserionwick <viserionwick@gmail.com>
2024-06-16 03:37:56 -0700

    fix: veriables

--- utils/veriables.ts
+++ utils/veriables.ts
@@ -1,8 +1,8 @@
-export const WEBSITE_URL =  process.env.NODE_ENV === "development" ?
+/* export const WEBSITE_URL =  process.env.NODE_ENV === "development" ?
                             "http://localhost:3000"
                             : process.env.NODE_ENV === "production" ?
                             process.env.WEBSITE_URL
-                            : ""
+                            : "" */
 
-export const GRAPHQL_API_ENDPOINT = `${WEBSITE_URL}/api/graphql`;
-export const AUTH_API_ENDPOINT = `${WEBSITE_URL}/api/auth`;
+export const GRAPHQL_API_ENDPOINT = `${process.env.WEBSITE_URL}/api/graphql`;
+export const AUTH_API_ENDPOINT = `${process.env.WEBSITE_URL}/api/auth`;
