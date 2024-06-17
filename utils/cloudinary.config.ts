da3de7cbed08f9d931713e030eabb931f5f94a27
viserionwick <viserionwick@gmail.com>
2024-06-17 03:31:54 -0700

    fix: en

--- utils/cloudinary.config.ts
+++ utils/cloudinary.config.ts
@@ -1,6 +1,6 @@
 export const cloudinaryConfig= {
   cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME!,
-  api_key: process.env.NEXT_CLOUDINARY_API_KEY!,
+  api_key: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY!,
   api_secret: process.env.CLOUDINARY_API_SECRET!,
   upload_url: `https://api.cloudinary.com/v1_1/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME!}/image/upload`,
   delete_url: `https://api.cloudinary.com/v1_1/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME!}/image/destroy`,
